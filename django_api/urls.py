464e1da654ce58113a5bbc68735aff9b98978d68
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-10 11:18:01 +0330

    add landing urls to url patterns

--- django_api/urls.py
+++ django_api/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('', include('landing.urls')),
 ]
 
 if settings.DEBUG:

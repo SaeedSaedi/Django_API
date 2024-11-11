09d9530dc9bab9f42dcdd67a5c1d9e289ba2d290
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-11 12:53:52 +0330

    add PostAdmin class for extra setting in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from blog.models import Post
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = "created_at"
+    empty_value_display = "-empty-"
+    list_display = ("title","created_at")
+    search_fields = ("title","content")
+    
+admin.site.register(Post,PostAdmin)

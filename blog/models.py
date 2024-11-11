059b1573486a0684d9caeeae1544b1a57cda43e4
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-11 10:13:23 +0330

    create a modol for posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True,blank=True)
+    updated_at = models.DateTimeField(auto_now=True,blank=True)
+    

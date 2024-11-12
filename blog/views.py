7eddc3767e50b6d609d34eb29d88dab31fd05a94
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-12 16:11:59 +0330

    filter post baser on published_at and dynamic counter_views (plus 1)

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+from blog.models import Post
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/home.html')
+    now = timezone.now()
+    post = Post.objects.filter(published_at__lte=now)
+    return render(request, 'blog/home.html',context={'posts':post})
 
-def post_single(request):
-    return render(request, 'blog/single.html')
+def post_single(request,post_id):
+    post = get_object_or_404(Post,id=post_id)
+    post.counted_views += 1
+    post.save()
+    return render(request, 'blog/single.html',context={'post':post})

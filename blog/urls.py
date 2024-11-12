66ede12b339f8fa579b50032d66eaf423d9646a0
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-12 15:17:54 +0330

    add simple template for blog post (main and single)

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from blog.views import index
+from blog.views import index,post_single
 
 urlpatterns = [
     path('', index),
+    path('post/', post_single),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/home.html')
+
+def post_single(request):
+    return render(request, 'blog/single.html')

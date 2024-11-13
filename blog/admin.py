f7a0e5c40228e3c099628b0c644efd97ff985314
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-13 17:07:49 +0330

    add author and status to list display

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy = "created_at"
     empty_value_display = "-empty-"
-    list_display = ("title","created_at")
+    list_display = ("title","author","status","created_at")
     search_fields = ("title","content")
     
 admin.site.register(Post,PostAdmin)

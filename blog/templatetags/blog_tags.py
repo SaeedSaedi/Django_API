e8c1e7fa40dab5a5fb7014318ec9ff4587c670ea
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-14 14:00:51 +0330

    chnage simple tag to return all post count

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,7 +1,9 @@
 from django import template
-
+from blog.models import Post
 register = template.Library()
 
-@register.simple_tag
-def hello():
-    return "hello"
+@register.simple_tag(name="totalposts")
+def total_posts():
+    
+    posts = Post.objects.filter(status=1).count()
+    return posts

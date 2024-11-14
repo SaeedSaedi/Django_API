b93a873068161a44d47508771e7a20f9bca31b0a
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-14 14:20:03 +0330

    create filder tag and inclusion tag

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -7,3 +7,17 @@ def total_posts():
     
     posts = Post.objects.filter(status=1).count()
     return posts
+
+@register.simple_tag(name="get_posts")
+def get_posts():
+    posts = Post.objects.filter(status=1).order_by('-created_on')
+    return posts
+
+@register.filter
+def snippet(v, n):
+    return  v[:n] + "..."
+
+@register.inclusion_tag('blog/popularposts.html')
+def popularposts():
+    posts = Post.objects.filter(status=1).order_by('-counted_views')[0:3]
+    return {'posts':posts}

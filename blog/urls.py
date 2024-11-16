b9bb821a3c320d0fcd7c9bb54d84d2298551817f
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-16 10:05:52 +0330

    add url for category

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from blog.views import index,post_single
+from blog.views import blog_category, index,post_single
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index),
+    path('', index,name='index'),
     path('posts/<int:post_id>/', post_single,name='post_detail'),
+    path('category/<str:cat_name>/', blog_category,name='category'),
 ]

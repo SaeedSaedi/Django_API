d13835046c7e6c02b9903d6dc8a2867bd83c47ba
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-24 13:07:25 +0330

    add contact us path

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from blog.views import blog_search, index, post_single
+from blog.views import blog_search, index, post_single, contact_us
 
 app_name = "blog"
 
 urlpatterns = [
     path("", index, name="index"),
+    path("contact-us", contact_us, name="contact"),
     path("posts/<int:post_id>/", post_single, name="post_detail"),
     path("category/<str:cat_name>/", index, name="category"),
     path("author/<str:username>/", index, name="author"),

5acae328dc774341d8c6049ffc61a601cb0d4652
saeed saedi <saeed.saedi.s@gmail.com>
2024-11-25 10:20:04 +0330

    register newsletter form to admin

--- landing/admin.py
+++ landing/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from landing.models import Contact
+from landing.models import Contact, Newsletter
+
 # Register your models here.
 
+
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'subject')
-    
+    list_display = ("name", "email", "subject")
+
+
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ("email",)
+
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Newsletter, NewsletterAdmin)
